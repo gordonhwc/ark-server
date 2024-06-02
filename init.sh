a6a674fe2409b69ed808cac4d5358ad27e562326
Gordon Hui <125633533+gordon0907@users.noreply.github.com>
2024-06-02 16:38:25 +0800

    Update init.sh
    - Correct the typo of `groupmod` to `usermod`.
    - Add `chmod` for changing ownership of the start script.

--- init.sh
+++ init.sh
@@ -2,11 +2,12 @@
 
 # Update the `steam` user with the specified UID and GID
 usermod --uid $UID steam
-groupmod --gid $GID steam
+usermod --gid $GID steam
 
 # Copy the start script to the volume if it doesn't already exist
 if [[ ! -f /home/steam/Steam/start.sh ]]; then
-    cat /home/steam/start.sh > /home/steam/Steam/start.sh
+    cp /home/steam/start.sh /home/steam/Steam/
+    chown steam:steam /home/steam/Steam/start.sh
 fi
 
 # Switch to the `steam` user and execute the start script
